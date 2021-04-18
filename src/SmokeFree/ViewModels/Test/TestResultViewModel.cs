eb63aba7f36a79e440e437cba772ee35bd900e58
s v <chofexx@gmail.com>
2021-04-18 11:08:51 +0300

    Implementing InitializeAsync

--- src/SmokeFree/ViewModels/Test/TestResultViewModel.cs
+++ src/SmokeFree/ViewModels/Test/TestResultViewModel.cs
@@ -6,6 +6,8 @@ using SmokeFree.Data.Models;
 using SmokeFree.Resx;
 using SmokeFree.ViewModels.AppSettings;
 using SmokeFree.ViewModels.Base;
+using System;
+using System.Linq;
 using System.Threading.Tasks;
 using Xamarin.CommunityToolkit.ObjectModel;
 
@@ -44,6 +46,72 @@ namespace SmokeFree.ViewModels.Test
 
             // Database
             _realm = realm;
+        }
+
+        #endregion
+
+        #region INIT
+
+        /// <summary>
+        /// Initialize View Model Initial State
+        /// </summary>
+        /// <param name="parameter">PARAM</param>
+        public override async Task InitializeAsync(object parameter)
+        {
+            try
+            {
+                // Get user
+                var userId = Globals.UserId;
+                var user = _realm.Find<User>(userId);
+
+                // Validate User
+                if (user != null)
+                {
+                    var testId = user.TestId;
+                    var userTest = user.Tests
+                        .FirstOrDefault(t => t.Id == testId);
+
+                    // Validate User Test
+                    if (userTest != null)
+                    {
+                        var testResult = userTest.CompletedTestResult;
+
+                        // Validate Test Result
+                        if (testResult != null)
+                        {
+                            this.TestResult = testResult;
+                        }
+                        else
+                        {
+                            // User Test Result Not Found!
+                            base._appLogger.LogCritical($"Can't find User Test Results: User id: {userId}, Test Id {testId}");
+
+                            await base.InternalErrorMessageToUser();
+                        }
+                    }
+                    else
+                    {
+                        // User Test Not Found!
+                        base._appLogger.LogCritical($"Can't find User Test: User id: {userId}, Test Id {testId}");
+
+                        await base.InternalErrorMessageToUser();
+                    }
+                }
+                else
+                {
+                    // User Not Found!
+                    base._appLogger.LogCritical($"Can't find User: User Id {userId}");
+
+                    await base.InternalErrorMessageToUser();
+                }
+            }
+            catch (Exception ex)
+            {
+
+                base._appLogger.LogError(ex.Message);
+
+                await base.InternalErrorMessageToUser();
+            }
         }
 
         #endregion
