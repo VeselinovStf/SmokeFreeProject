942ba32cbcc26a7ffcf522173e4ed83558995cae
s v <chofexx@gmail.com>
2021-04-18 10:50:12 +0300

    Adding Base Implementation

--- src/SmokeFree/ViewModels/Test/TestResultViewModel.cs
+++ src/SmokeFree/ViewModels/Test/TestResultViewModel.cs
@@ -1,8 +1,13 @@
-﻿using SmokeFree.Abstraction.Services.General;
+﻿using Realms;
+using SmokeFree.Abstraction.Services.General;
 using SmokeFree.Abstraction.Utility.Logging;
 using SmokeFree.Abstraction.Utility.Wrappers;
+using SmokeFree.Data.Models;
 using SmokeFree.Resx;
+using SmokeFree.ViewModels.AppSettings;
 using SmokeFree.ViewModels.Base;
+using System.Threading.Tasks;
+using Xamarin.CommunityToolkit.ObjectModel;
 
 namespace SmokeFree.ViewModels.Test
 {
@@ -13,12 +18,22 @@ namespace SmokeFree.ViewModels.Test
     {
         #region FIELDS
 
+        /// <summary>
+        /// Test Results
+        /// </summary>
+        private TestResult _testResult;
+
+        /// <summary>
+        /// Database
+        /// </summary>
+        private readonly Realm _realm;
 
         #endregion
 
         #region CTOR
 
         public TestResultViewModel(
+            Realm realm,
             INavigationService navigationService,
             IDateTimeWrapper dateTimeWrapper,
             IAppLogger appLogger,
@@ -26,6 +41,43 @@ namespace SmokeFree.ViewModels.Test
         {
             // Set View Title
             ViewTitle = AppResources.TestResultViewModelTiitle;
+
+            // Database
+            _realm = realm;
+        }
+
+        #endregion
+
+        #region COMMANDS
+
+        /// <summary>
+        /// Navigate to settings View
+        /// </summary>
+        public IAsyncCommand OnSettingsCommand => new AsyncCommand(
+            ExecuteNavigateToSetting,
+            onException: base.GenericCommandExeptionHandler,
+            allowsMultipleExecutions: false);
+
+        private async Task ExecuteNavigateToSetting()
+        {
+            await base._navigationService.NavigateToAsync<AppSettingsViewModel>();
+        }
+
+        #endregion
+
+        #region PROPS
+
+        /// <summary>
+        /// Current Display Test Results
+        /// </summary>
+        public TestResult TestResult
+        {
+            get { return _testResult; }
+            set
+            {
+                _testResult = value;
+                OnPropertyChanged();
+            }
         }
 
         #endregion
