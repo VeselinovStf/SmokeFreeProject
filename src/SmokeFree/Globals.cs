93aa70f20a484b051d9bb5452592d343f37dad5c
Stefan Veselinov <chofexx@gmail.com>
2021-05-02 07:44:24 +0300

    Moving Views Titles To Top

--- src/SmokeFree/Globals.cs
+++ src/SmokeFree/Globals.cs
@@ -10,7 +10,7 @@ namespace SmokeFree
         /// <summary>
         /// Used For Testing 
         /// </summary>
-        public static bool MockRun = false;
+        public static bool MockRun = true;
 
         // Notification Ids
         public static int TestingTimeNotificationId = 666;
