99dec50230b01114de71e5d2e00a4425963f84fc
Stefan Veselinov <chofexx@gmail.com>
2021-05-02 12:25:04 +0300

    Changing Smoke Cigar Limit To 7 min

--- src/SmokeFree/Globals.cs
+++ src/SmokeFree/Globals.cs
@@ -13,7 +13,7 @@ namespace SmokeFree
         /// <summary>
         /// Used For Testing 
         /// </summary>
-        public static bool MockRun = true;
+        public static bool MockRun = false;
 
         // Notification Ids
         public static int TestingTimeNotificationId = 666;
@@ -79,7 +79,7 @@ namespace SmokeFree
         /// <summary>
         /// Limits if User Forgets To push Smoke Done
         /// </summary>
-        public static int OneSmokeTreshHoldTimeMinutes = 9;
+        public static int OneSmokeTreshHoldTimeMinutes = 7;
 
         /// <summary>
         /// Minimum Limit for Challenge in Days
