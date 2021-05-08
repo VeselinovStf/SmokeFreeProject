d1d3840e544905bbaa83ed1d5dda0345202e0c5f
Stefan Veselinov <chofexx@gmail.com>
2021-05-08 09:11:21 +0300

    Updating App  Style

---
+++ src/SmokeFree/Views/AppSettings/StyleAppSetting.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace SmokeFree.Views.AppSettings
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class StyleAppSetting : ResourceDictionary
+	{
+		public StyleAppSetting ()
+		{
+			InitializeComponent ();
+		}
+	}
+}
